0e7fbd206ef07dc3cd8930db89f22e66a5121b3b
PenguinRage <3924194+PenguinRage@users.noreply.github.com>
2025-03-03 23:06:44 +1100

    COMMIT ALL THE FILES!

---
+++ go_redis/redis_app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+// ServerClient function  
+func ServerClient() {
+	up_token := os.Getenv("UP_API_KEY")
+	server_name := os.Getenv("SERVER_NAME")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     server_name + ":6379",
+		Password: "",
+		DB:       0,
+	})
+
+	err := rdb.Set(ctx, "up_token", up_token, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "hello").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(val)
+
+}
+
+// main function  
+func main() {
+	fmt.Println("Testing Redis app...")
+
+	ServerClient()
+}
