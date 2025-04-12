bdc98d34d1535b89fc0a76990cf885e874974ea2
PenguinRage <3924194+PenguinRage@users.noreply.github.com>
2025-04-12 23:11:29 +1000

    what the hell happened here

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type album struct {
@@ -22,6 +23,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/album/:id", getAlbumByID)
 	router.POST("/albums", postAlbum)
 	router.Run("localhost:8080")
 }
@@ -41,4 +43,16 @@ func postAlbum(c *gin.Context) {
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
