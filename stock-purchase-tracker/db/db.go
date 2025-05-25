ba7e09ff73a54d690b893a4a6b3e9de39c21df78
PenguinRage <3924194+PenguinRage@users.noreply.github.com>
2025-05-26 07:56:09 +1000

    I just wanna tell you how I'm feeling

--- stock-purchase-tracker/db/db.go
+++ stock-purchase-tracker/db/db.go
@@ -8,10 +8,14 @@ import (
 )
 
 func InsertStock(stock *datatypes.Stock) {
-	db, err := sqlx.Connect("postgres", "user=foo dbname=bar sslmode=disable")
+	db, err := sqlx.Connect("postgres", "user=icleasby dbname=stocks sslmode=disable")
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	defer db.Close()
+
+	db.MustExec("INSERT INTO stocks (exchange, code, state, quantity, purchase_price) VALUES ($1, $2, $3, $4, $5)", stock.Exchange, stock.Code, stock.State, stock.Quantity, stock.Price)
 
 	db.Close()
 
